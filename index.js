3cd43e97f8e60397e93a1598cb18c32c24d981ba
Amaresh Sahoo <41888623+AmareshSahoo@users.noreply.github.com>
2022-01-28 13:03:24 +0530

    update

--- index.js
+++ index.js
@@ -11,4 +11,7 @@ app.use(cors({ origin: 'http://localhost:4200' }));
 
 app.listen(process.env.PORT || 3000, () => console.log('Server started at port : 3000'));
 
+app.use('/test', (req, res) => {
+    res.send('Hello World!')
+})
 app.use('/employees', employeeController);
